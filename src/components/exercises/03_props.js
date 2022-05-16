ad23ab480a3220013b13807306fae2199f1e0f7b
Bryan Keane <20089166@mail.wit.ie>
2022-05-16 17:11:57 +0100

    Exercise 3 completed

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
-const Demo = () => {
+const Demo = (props) => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{props.className}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.name}</td>
+            <td>{props.noLectures}</td>
+            <td>{props.noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{props.name}</td>
+            <td>{props.noLectures}</td>
+            <td>{props.noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/components/samples/03_props.js
+++ src/components/samples/03_props.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Demo = (props) => {
-    return (
-      <div > 
-        <h2>{`${props.heading} Languages`}</h2>
-        <ul> 
-          <li>{props.languages[0]}</li>
-          <li>{props.languages[1]} </li>
-          <li>{props.languages[2]} </li>
-        </ul>
-      </div>
-    );
-  
-}
+  return (
+    <div>
+      <h2>{`${props.heading} Languages`}</h2>
+      <ul>
+        <li>{props.languages[0]}</li>
+        <li>{props.languages[1]} </li>
+        <li>{props.languages[2]} </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Demo
+export default Demo;
