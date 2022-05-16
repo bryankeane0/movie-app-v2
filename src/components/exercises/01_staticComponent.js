3014a4d86a2a054f604d6ef5f8fbbfb727267fcf
Bryan Keane <20089166@mail.wit.ie>
2022-05-16 16:57:08 +0100

    Exercise 1 Completed

---
+++ src/components/exercises/01_staticComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+
+const Demo = () => {
+  return (
+    <div>
+      <h2>HDip Computer Science - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>DevOps</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Enterprise Web Dev</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Demo;
