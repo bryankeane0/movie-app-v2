bd7769f1b31334fe307925d4fbe224eaf3934f9c
Bryan <bryankeane0@gmail.com>
2022-05-29 13:29:30 +0100

    Movie details page rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from "./pages/movieDetailsPage"
+
+const images = [
+    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const sample = {
   adult: false,
@@ -75,7 +83,6 @@ const sample = {
       name: "English",
     },
   ],
-  status: "Released",
   tagline: "Darkness rises... and light to meet it",
   title: "Star Wars: The Last Jedi",
   video: false,
@@ -86,9 +93,9 @@ const sample = {
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
-  return (
-      <HomePage movies={movies} />
-  );
-};
+    return (
+        <MovieDetailsPage movie={sample} images={images} />
+        );
+  };
 
 ReactDOM.render(<App />, document.getElementById("root"));
