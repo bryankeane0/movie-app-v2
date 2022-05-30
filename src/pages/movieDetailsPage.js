84cef6aee59df29d7ea7a649d9273c3cf2f40289
Bryan <bryankeane0@gmail.com>
2022-05-30 13:58:44 +0100

    Refactor: template list page; HTTP API calls centralized

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -5,7 +5,8 @@ import MovieDetails from "../components/movieDetails/";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import ImageList from "@material-ui/core/ImageList";
-import { ImageListItem } from '@material-ui/core'
+import { ImageListItem } from '@material-ui/core';
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,26 +27,15 @@ const MoviePage = (props) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        setMovie(movie);
-      });
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        setImages(images);
-      });
+    getMovieImages(id).then((images) => {
+      setImages(images);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
