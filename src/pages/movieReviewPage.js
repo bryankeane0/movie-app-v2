2c74f40fa761eacffa1efb73973ce0f82185911a
Bryan <bryankeane0@gmail.com>
2022-05-31 20:26:53 +0100

    Full review page

---
+++ src/pages/movieReviewPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PageTemplate from "../components/templateMoviePage";
+import MovieReview from "../components/movieReview";
+
+const MovieReviewPage = (props) => {
+  const {movie, review} = props.location.state
+  return (
+    <PageTemplate movie={movie}>
+      <MovieReview review={review} />
+    </PageTemplate>
+  );
+};
+
+export default MovieReviewPage
