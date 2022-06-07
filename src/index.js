822ec58392640d34feeb16cf81f446a8303323ba
Bryan <bryankeane0@gmail.com>
2022-06-07 12:15:01 +0100

    Removed unused link import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes , Route, Navigate, Link } from "react-router-dom";
+import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
