fb02ba76485d2126c436a92a971f7d6ed52f36b5
Bryan Keane <bkeane@redhat.com>
2022-06-09 20:07:13 +0100

    Fixing minor deprecation bugs

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
 
 const useStyles = makeStyles((theme) => ({
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
     justifyContent: "space-around",
   },
-  gridList: {
+  imageList: {
     width: 450,
     height: '100vh',
   },
@@ -36,16 +36,16 @@ const TemplateMoviePage = ({ movie, children }) => {
       <Grid container spacing={5} style={{ padding: "15px" }}>
         <Grid item xs={3}>
           <div className={classes.root}>
-            <GridList cellHeight={500} className={classes.gridList} cols={1}>
+            <ImageList rowHeight={500} className={classes.imageList} cols={1}>
               {images.map((image) => (
-                <GridListTile key={image.file_path} className={classes.gridListTile} cols={1}>
+                <ImageListItem key={image.file_path} className={classes.imageListItem} cols={1}>
                   <img
                     src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
                     alt={image.poster_path}
                   />
-                </GridListTile>
+                </ImageListItem>
               ))}
-            </GridList>
+            </ImageList>
           </div>
         </Grid>
 
