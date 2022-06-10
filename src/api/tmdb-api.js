d6ff088870dc34826f48f4aa72444ab039c483cc
Bryan Keane <bkeane@redhat.com>
2022-06-10 16:57:07 +0100

    Changed upcoming movies to use caching and added better error handling for api calls

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -65,18 +65,29 @@ export const getMovieImages = ({ queryKey }) => {
 export const getMovieReviews = (id) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
+      ).then( (response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+    
+      })
+      .catch((error) => {
+        throw error
+     });
   };
 
-  export const getUpcoming = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+    });
 };
