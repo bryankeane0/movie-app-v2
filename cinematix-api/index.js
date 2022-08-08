68e3c074117d21f5e877c07ea14e85d6cf74739c
Bryan Keane <bkeane@redhat.com>
2022-08-08 19:45:49 +0100

    Topic 10 - session based auth

--- cinematix-api/index.js
+++ cinematix-api/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 import './db';
 import './seedData';
 
@@ -21,8 +23,13 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(express.json());
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
