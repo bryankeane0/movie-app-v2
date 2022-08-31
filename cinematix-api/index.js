d0c05636f5c8d84c2b34b252c8d56d902a4b5dd1
Bryan Keane <bkeane@redhat.com>
2022-08-31 20:31:23 +0100

    Added some custom api work for shows and actors

--- cinematix-api/index.js
+++ cinematix-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import actorsRouter from './api/actors';
+import showsRouter from './api/shows';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import passport from './authenticate';
@@ -25,7 +27,12 @@ const port = process.env.PORT;
 app.use(passport.initialize());
 
 app.use(express.json());
-app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/movies/discover', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/movies/upcoming', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/tv/discover', passport.authenticate('jwt', {session: false}), showsRouter);
+app.use('/tv/toprated', passport.authenticate('jwt', {session: false}), showsRouter);
+app.use('/actors/popular', passport.authenticate('jwt', {session: false}), actorsRouter);
+app.use('/actors/latest', passport.authenticate('jwt', {session: false}), actorsRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
